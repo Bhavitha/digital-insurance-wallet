bfe203d6cdc409a3cd835ee0e10b7db88757e4cc
Bhavitha <bhavitagottimukkula@gmail.com>
2025-07-16 00:24:05 +0530

    Initial Commit: Added Xcode project and default files

---
+++ InsureWallet/InsureWallet/InsureWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsureWalletApp.swift
+//  InsureWallet
+//
+//  Created by Bhavitha Gottimukkula on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct InsureWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
