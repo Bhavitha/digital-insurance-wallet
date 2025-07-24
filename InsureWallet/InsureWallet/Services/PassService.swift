a02a62396ff7ca1acf27159e3765d87732f9112b
Bhavitha <bhavitagottimukkula@gmail.com>
2025-07-24 17:20:29 +0530

    remove commented code

--- InsureWallet/InsureWallet/Services/PassService.swift
+++ InsureWallet/InsureWallet/Services/PassService.swift
@@ -16,33 +16,6 @@ class PassService: NSObject {
         PKAddPassesViewController.canAddPasses()
     }
 
-    // load pass from bundle
-//    func loadPass(cardNo: String) -> PKPass? {
-//        var passName: String?
-//        
-//        if cardNo == "1" {
-//            passName = "pass"
-//        } else if cardNo == "2" {
-//            passName = "Aviva"
-//        } else if cardNo == "3" {
-//            passName = "Lic"
-//        }
-//        
-//        guard let url = Bundle.main.url(forResource: passName, withExtension: "pkpass") else {
-//            print("❌ pass.pkpass file not found in bundle")
-//            return nil
-//        }
-//
-//        do {
-//            let passData = try Data(contentsOf: url)
-//            let pass = try PKPass(data: passData)
-//            return pass
-//        } catch {
-//            print("❌ Failed to load PKPass: \(error)")
-//            return nil
-//        }
-//    }
-    
     //load pass from server
     func requestPass(from urlString: String, with parameters: [String: Any], completion: @escaping (PKPass?) -> Void) {
         guard let request = createRequest(urlString: urlString, parameters: parameters) else {
@@ -97,22 +70,6 @@ class PassService: NSObject {
            return request
        }
 
-//    func presentWalletPass(cardNo: String) {
-//        guard let pass = loadPass(cardNo: cardNo),
-//              let addVC = PKAddPassesViewController(pass: pass) else {
-//            print("❌ Wallet VC not available")
-//            return
-//        }
-//
-//        if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-//           let window = scene.windows.first,
-//           let rootVC = window.rootViewController {
-//            DispatchQueue.main.async {
-//                rootVC.present(addVC, animated: true)
-//            }
-//        }
-//    }
-    
     func presentWalletPass(with parameters: [String: Any]) {
         let urlString: String = "https://3d372ef31224.ngrok-free.app/wallet-service/create-apple-pass"
            requestPass(from: urlString, with: parameters) { pass in
@@ -122,7 +79,6 @@ class PassService: NSObject {
                    return
                }
                self.pkPass = pass
-//               addVC.delegate = self
                
                DispatchQueue.main.async {
                    if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
@@ -134,22 +90,3 @@ class PassService: NSObject {
            }
        }
 }
-
-//extension PassService: PKAddPassesViewControllerDelegate {
-//    
-//    func addPassesViewControllerDidFinish(_ controller: PKAddPassesViewController) {
-//        let passLib = PKPassLibrary()
-//        guard let passDetails = self.pkPass else { return }
-//        
-//        let arrayOfPasses = passLib.passes()
-//        print("The count of passes are: ", arrayOfPasses.count)
-//        
-//        if passLib.containsPass(passDetails) {
-//            // Its added in wallet
-//            print("Its added in wallet")
-//        } else {
-//            // not present in wallet
-//            print("not present in wallet")
-//        }
-//    }
-//}
